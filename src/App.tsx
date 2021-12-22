568ae6e802a395f5615a9167d54e9e98fbca2d95
yoleo <yoleo@yandex.ru>
2021-12-23 01:52:33 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { ThemeProvider } from './ui/theme';
+import { EditorTemplate } from './ui/templates/editor';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <EditorTemplate 
+        
+      />
+    </ThemeProvider>
+  );
+}
+
+export default App;
