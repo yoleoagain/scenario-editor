a24b2da1980964626dff32aa5adaf1a1ff7ab734
yoleo@yandex.ru <yoleo@yandex.ru>
2021-12-24 11:50:39 +0200

    rxjs

--- src/store/editor.ts
+++ src/store/editor.ts
@@ -3,6 +3,7 @@ import { EditorState } from 'draft-js'
 import { Subject } from 'rxjs'
 import React from 'react';
 //https://www.youtube.com/watch?v=_cjGaeCASqs&ab_channel=WebBlocksApp
+//https://medium.com/disney-streaming/react-rxjs-pushing-state-down-4205af78f8b0
 type SetEditorState = React.Dispatch<React.SetStateAction<EditorState>>
 const subject = new Subject<EditorState>()
 let state = EditorState.createEmpty()
