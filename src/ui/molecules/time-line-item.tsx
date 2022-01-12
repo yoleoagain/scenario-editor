98dae5361b22be0536a8c0ac71a57f62b64bfffe
yoleo <yoleo@yandex.ru>
2022-01-13 01:27:21 +0200

    Split time line item in separate components

---
+++ src/ui/molecules/time-line-item.tsx
@@ -0,0 +1,34 @@
+﻿import React from 'react'
+import styled from 'styled-components'
+import { createAutoTiming, Timing } from 'ui/utils/time-line'
+
+type Props = { item:Timing }
+
+const TimingItem = styled.div`
+  color: #000;
+  font-size: 14px;
+  color: ${({ theme }) => theme.palette.mainColor};
+  background: ${({ theme }) => theme.palette.mainLighter};
+  text-align: center;
+`
+
+export const TimeLineItem: React.FC<Props> = ({ item }) => {
+  let style = {};
+
+  if (item?.rowNode && 'getComputedStyle' in window){
+    const { rowNode } = item
+    const { paddingTop, paddingBottom, marginBottom, marginTop } = getComputedStyle(rowNode)
+
+    // console.log(paddingTop, paddingBottom, marginBottom, marginTop, rowNode.offsetHeight)
+    
+    style = {
+      height: `calc(${paddingTop} + ${paddingBottom} + ${marginTop} + ${marginBottom} + ${rowNode.offsetHeight}px)`,
+    }
+  }
+
+  return (
+    <TimingItem style={style}>
+      {item.isEmptyRow ? '' : item.timeLabel}
+    </TimingItem>
+  )
+}
--- src/ui/organisms/TimeLine.tsx
+++ src/ui/organisms/TimeLine.tsx
@@ -3,8 +3,9 @@ import styled from 'styled-components'
 import { Column } from '../atoms/Column'
 import { editoreStore$ } from 'store/editor'
 import { useObservableState } from 'observable-hooks'
-import { createAutoTiming, Timing } from 'ui/utils/time-line'
+import { createAutoTiming } from 'ui/utils/time-line'
 import { getEditorText } from 'ui/utils/editor'
+import { TimeLineItem } from '../molecules/time-line-item'
 
 type Props = {}
 
@@ -14,43 +15,14 @@ const Wrap = styled(Column)`
   background: ${({ theme }) => theme.palette.main};
   min-height: calc(100vh - 40px);
 `
-const TimingItem = styled.div`
-  color: #000;
-  font-size: 14px;
-  color: ${({ theme }) => theme.palette.mainColor};
-  background: ${({ theme }) => theme.palette.mainLighter};
-  text-align: center;
-  /* min-height: 20.8px; */
-`
-
 
 export const TimeLine: React.FC<Props> = () => {
   const editorState = useObservableState(editoreStore$, editoreStore$.value)
   const timings = createAutoTiming(getEditorText(editorState))
   
   return (
-    // TODO: Replace timing item in separate component
     <Wrap>
-      {timings.map(t => {
-        let style = {};
-
-        if (t?.rowNode && 'getComputedStyle' in window){
-          const { rowNode } = t
-          const { paddingTop, paddingBottom, marginBottom, marginTop } = getComputedStyle(rowNode)
-
-          // console.log(paddingTop, paddingBottom, marginBottom, marginTop, rowNode.offsetHeight)
-          
-          style = {
-            height: `calc(${paddingTop} + ${paddingBottom} + ${marginTop} + ${marginBottom} + ${rowNode.offsetHeight}px)`,
-          }
-        }
-
-        return (
-          <TimingItem style={style}>
-            {t.isEmptyRow ? '' : t.timeLabel}
-          </TimingItem>
-        )
-      })}
+      {timings.map((t, i) => <TimeLineItem key={`time-line-${i}`} item={t} />)}
     </Wrap>
   )
 }
